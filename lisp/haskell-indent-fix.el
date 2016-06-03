94764757111f0b0f56d5bfcb72e9494c0dbf5f96
Troy Pracy <troyp7@gmail.com>
2016-06-03 18:56:19 +1000

    new file

---
+++ lisp/haskell-indent-fix.el
@@ -0,0 +1,45 @@
+;; https://github.com/haskell/haskell-mode/issues/896#issuecomment-185945576
+
+(defun haskell-indentation-indent-line ()
+  "Indent current line, cycle though indentation positions.
+Do nothing inside multiline comments and multiline strings.
+Start enumerating the indentation points to the right.  The user
+can continue by repeatedly pressing TAB.  When there is no more
+indentation points to the right, we switch going to the left."
+  (interactive)
+    ;; try to repeat
+  (when (not (haskell-indentation-indent-line-repeat))
+    (setq haskell-indentation-dyn-last-direction nil)
+    ;; parse error is intentionally not caught here, it may come from
+    ;; `haskell-indentation-find-indentations', but escapes the scope
+    ;; and aborts the operation before any moving happens
+    (let* ((cc (current-column))
+           (ci (haskell-indentation-current-indentation))
+           (inds (save-excursion
+                   (move-to-column ci)
+                   (or (haskell-indentation-find-indentations)
+                       '(0))))
+           (valid (memq ci inds))
+           (cursor-in-whitespace (< cc ci))
+           (evil-special-command? (and (bound-and-true-p evil-mode)
+                                       (memq this-command '(evil-open-above
+                                                            evil-open-below
+                                                            evil-replace))))
+           (on-last-indent (eq ci (car (last inds)))))
+      (if (and valid cursor-in-whitespace)
+          (move-to-column ci)
+        (haskell-indentation-reindent-to
+         (funcall
+          (if on-last-indent
+              #'haskell-indentation-previous-indentation
+            #'haskell-indentation-next-indentation)
+          (if evil-special-command?
+              (save-excursion
+                (end-of-line 0)
+                (1- (haskell-indentation-current-indentation)))
+            ci)
+          inds
+          'nofail)
+         cursor-in-whitespace))
+      (setq haskell-indentation-dyn-last-direction (if on-last-indent 'left 'right)
+            haskell-indentation-dyn-last-indentations inds))))
